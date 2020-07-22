497e5ce01afc0e3064e7b18eb2129ebd9f1785e7
Jonathon Aslanian <aslanianj@gmail.com>
2020-07-22 14:40:01 -0400

    Nope, turns out it just doesn't like literals! Casting to bool just so it won't complain.

--- Django-Tatorr/settings.py
+++ Django-Tatorr/settings.py
@@ -25,12 +25,10 @@ SECRET_KEY = os.environ.get('DJANGO-TATORR_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Check Heroku environment first, otherwise enabled debug
-if os.environ.get('DJANGO_DEBUG') is "False":
-    DEBUG = os.environ.get('DJANGO_DEBUG')
+if os.environ.get('DJANGO_DEBUG') == "False":
+    DEBUG = bool(os.environ.get('DJANGO_DEBUG'))
 else:
     DEBUG = True
-
-
 # Set to Heroku to prevent HTTP Host header attacks
 ALLOWED_HOSTS = ['.herokuapp.com']
 

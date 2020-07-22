9205943919a5fdaa22225be3245b61cd7e48d9a9
Cha0sDunk <orrkw.kurtis@gmail.com>
2020-07-22 01:59:47 -0400

    Fix names for new repo

--- Django-Tatorr/wsgi.py
+++ Django-Tatorr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoTatorr project.
+WSGI config for Django-Tatorr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTatorr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-Tatorr.settings')
 
 application = get_wsgi_application()

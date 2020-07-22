3b001317632f6ce7f89681bae2b32651a80c1332
Jonathon Aslanian <aslanianj@gmail.com>
2020-07-22 12:23:36 -0400

    Added fallback for SECRET_KEY if there is no variable found.

--- Django-Tatorr/settings.py
+++ Django-Tatorr/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO-TATORR_SECRET_KEY']
+# SECURITY WARNING: Environ variable for production - fallback for dev. Dev secret is never used in production.
+SECRET_KEY = os.environ.get('DJANGO-TATORR_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

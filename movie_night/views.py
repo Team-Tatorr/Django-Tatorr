c20b4bba4b4b7dcd2a8e6ad6e7d32be27bd5f618
Jon <aslanianj@gmail.com>
2020-07-27 21:49:24 -0400

    gitflow-hotfix-stash: url-syntax-error

--- movie_night/views.py
+++ movie_night/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 
-# Create your views here.
-def movie_calendar(request):
-    return render(request, 'moviecal.html', {})
+
+def calendar(request):
+    # Create full HTML Cal - This is from <body> to </body>
+    cal = create_cal()
+    return render(request, 'movie_night/base_content.html', {'calendar': mark_safe(cal), })
